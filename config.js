f293a1afcc135b1deb4bb4d078448c3e17dbd7d9
tarun_mangodata <tarun@mangodata.co>
2018-02-23 15:04:29 +0530

    final commit

--- config.js
+++ config.js
@@ -2,7 +2,7 @@
 module.exports = {'port':3000,
 									'FUEL_PRTROL':'petrol',
 									'FUEL_DIESEL':'diesel',
-									'GOOGLE_API_KEY':'GOOGLE_API_KEY',
+									'GOOGLE_API_KEY':'AIzaSyDKPfHzj3bzyKiVHKx5eFt-a-UKdnVLAlY',
 								 	'FUEL_PRICE_API_URL':'https://fuelpriceindia.herokuapp.com/price?city=CITYNAME&fuel_type=FUELTYPE',
 								 	'PLACE_DETAILS_API_URL':'https://maps.googleapis.com/maps/api/place/details/json?placeid=USER_QUERY&key=GOOGLE_API_KEY'
 };
